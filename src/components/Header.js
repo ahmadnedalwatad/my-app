7a72eeaee4e322a8b66e2954c6713d860ca54d73
ahmadnedalwatad <ahm2d.w@gmail.com>
2023-08-01 09:15:40 +0300

    app designed

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from "../assets/Logo.svg";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Header</h1>
+      <img src={logo} alt="logo" />
+    </>
+  );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+export default function Nav() {
+  return (
+    <>
+      <h1>Nav</h1>
+      <ul>
+        <li>
+          <a href="https://www.w3schools.com">link 1</a>
+        </li>
+        <li>
+          <a href="https://www.w3schools.com">link 2</a>
+        </li>
+        <li>
+          <a href="https://www.w3schools.com">link 3</a>
+        </li>
+        <li>
+          <a href="https://www.w3schools.com">link 4</a>
+        </li>
+      </ul>
+    </>
+  );
+}
